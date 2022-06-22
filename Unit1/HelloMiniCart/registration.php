166b09d47fcb742e3e0146dc119303547fd30e33
sushil <sushilcoder504@gmail.com>
2022-06-22 21:25:40 +0530

    initialstage

---
+++ Unit1/HelloMiniCart/registration.php
@@ -0,0 +1,12 @@
+<?php
+// Exercise: 1.3.2.1
+/**
+ * Copyright © Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Unit1_HelloMiniCart',
+    __DIR__
+);
